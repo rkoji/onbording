f1e84eaec13cbae15a5da7f8c64e6623e7e9053d
rkoji <wl0wn1212@gmail.com>
2024-11-07 16:22:58 +0900

    [Feat] init commit

---
+++ src/main/java/com/onbording/auth/OnbordingApplication.java
@@ -0,0 +1,13 @@
+package com.onbording.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnbordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnbordingApplication.class, args);
+	}
+
+}
